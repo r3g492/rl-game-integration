95da1f90f9c0caddcc0245475d96d7e4454cdcb0
r3g492 <kangggunw3492@gmail.com>
2025-06-16 22:59:22 +0900

    init grid

--- output/drawing.go
+++ output/drawing.go
@@ -8,5 +8,16 @@ func DrawOutput() {
 	rl.ClearBackground(rl.RayWhite)
 	rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
 
+	rl.BeginMode3D(
+		rl.NewCamera3D(
+			rl.Vector3{X: 10, Y: 10, Z: 10},
+			rl.Vector3{X: 0, Y: 0, Z: 0},
+			rl.Vector3{X: 0, Y: 1, Z: 0},
+			90,
+			rl.CameraPerspective,
+		),
+	)
+
+	rl.DrawGrid(100, 1)
 	rl.EndDrawing()
 }
