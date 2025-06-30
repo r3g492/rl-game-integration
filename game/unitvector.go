70cf89b79fe824c9f7f2a383636b90940cb32594
r3g492 <kangggunw3492@gmail.com>
2025-07-01 00:14:25 +0900

    delete meaningless code

--- game/unitvector.go
+++ game/unitvector.go
@@ -1,20 +1,11 @@
 package game
 
 import (
-	"errors"
 	"math"
 )
 
 type UnitVector struct {
 	X, Y, Z float32
-}
-
-func NewUnitVector(x, y, z float32) (UnitVector, error) {
-	sqrt := float32(math.Sqrt(float64(x*x + y*y + z*z)))
-	if sqrt == 0 {
-		return UnitVector{}, errors.New("cannot create unit vector from zero vector")
-	}
-	return UnitVector{X: x / sqrt, Y: y / sqrt, Z: z / sqrt}, nil
 }
 
 func Cross(a, b UnitVector) UnitVector {
