9c742aa3e6e603b8c5f5cd123384f558a442a42a
r3g492 <kangggunw3492@gmail.com>
2025-07-05 00:25:04 +0900

    add rps model

--- go.mod
+++ go.mod
@@ -3,8 +3,21 @@ module war-game-poc
 go 1.24
 
 require (
+	github.com/chewxy/hm v1.0.0 // indirect
+	github.com/chewxy/math32 v1.0.4 // indirect
 	github.com/ebitengine/purego v0.8.4 // indirect
 	github.com/gen2brain/raylib-go/raylib v0.0.0-20250521210303-fca3bf26c568 // indirect
+	github.com/gogo/protobuf v1.3.0 // indirect
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/flatbuffers v1.11.0 // indirect
+	github.com/owulveryck/onnx-go v0.5.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/xtgo/set v1.0.0 // indirect
+	github.com/yalue/onnxruntime_go v1.20.0 // indirect
 	golang.org/x/exp v0.0.0-20250531010427-b6e5de432a8b // indirect
 	golang.org/x/sys v0.33.0 // indirect
+	gonum.org/v1/gonum v0.0.0-20190902003836-43865b531bee // indirect
+	gorgonia.org/tensor v0.9.3 // indirect
+	gorgonia.org/vecf32 v0.9.0 // indirect
+	gorgonia.org/vecf64 v0.9.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"war-game-poc/game"
-	"war-game-poc/input"
-	"war-game-poc/output"
+	ort "github.com/yalue/onnxruntime_go"
+	"log"
 )
 
 func main() {
-	output.InitWindow(1600, 900)
+	/*output.InitWindow(1600, 900)
 	defer output.CloseWindow()
 
 	var player = game.CreatePlayer()
@@ -33,5 +32,55 @@ func main() {
 			player,
 		)
 
+	}*/
+
+	ort.SetSharedLibraryPath("./libonnxruntime/libonnxruntime.so")
+	err := ort.InitializeEnvironment()
+	if err != nil {
+		panic(err)
 	}
+	defer ort.DestroyEnvironment()
+
+	// Input: int32[1,1]
+	inputData := []int32{1} // 0: Rock, 1: Paper, 2: Scissors
+	inputShape := ort.NewShape(1, 1)
+	inputTensor, err := ort.NewTensor(inputShape, inputData)
+	if err != nil {
+		log.Fatalf("failed to create input tensor: %v", err)
+	}
+	defer inputTensor.Destroy()
+
+	// Output: int64[1]
+	outputShape := ort.NewShape(1)
+	outputTensor, err := ort.NewEmptyTensor[int64](outputShape)
+	if err != nil {
+		log.Fatalf("failed to create output tensor: %v", err)
+	}
+	defer outputTensor.Destroy()
+
+	// Session (input/output names from Netron: "input" and "output")
+	session, err := ort.NewAdvancedSession("./rps_agent_visible.onnx",
+		[]string{"input"}, []string{"output"},
+		[]ort.Value{inputTensor}, []ort.Value{outputTensor}, nil)
+	if err != nil {
+		log.Fatalf("failed to create session: %v", err)
+	}
+	defer session.Destroy()
+
+	// Run inference
+	err = session.Run()
+	if err != nil {
+		log.Fatalf("failed to run session: %v", err)
+	}
+
+	// Get output values (predicted action)
+	outputVals := outputTensor.GetData()
+	fmt.Printf("Predicted action index: %d\n", outputVals[0])
+	actions := []string{"Rock", "Paper", "Scissors"}
+	if int(outputVals[0]) >= 0 && int(outputVals[0]) < len(actions) {
+		fmt.Printf("Predicted action: %s\n", actions[outputVals[0]])
+	} else {
+		fmt.Printf("Invalid action index: %d\n", outputVals[0])
+	}
+
 }
