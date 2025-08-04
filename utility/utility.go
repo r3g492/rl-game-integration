acab3bd2fa074bc12b8eaff5a8c8d9ced7a8e715
r3g492 <kangggunw3492@gmail.com>
2025-08-05 00:58:13 +0900

    add utility

---
+++ utility/utility.go
@@ -0,0 +1,27 @@
+package utility
+
+func Clamp(x, min, max float32) float32 {
+	if x < min {
+		return min
+	}
+	if x > max {
+		return max
+	}
+	return x
+}
+
+func Suppress(x, lower, upper float32) float32 {
+	if x > lower && x < upper {
+		return 0
+	}
+	return x
+}
+
+func Friction(x, delta float32) float32 {
+	if x > delta {
+		return x - delta
+	} else if x < -delta {
+		return x + delta
+	}
+	return 0
+}
